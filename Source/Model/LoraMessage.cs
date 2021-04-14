4faacaddcb72ea8868c466a6995c483213fd56f0
srudaa <sigbjorn.rudaa@raalabs.com>
2021-04-14 12:45:22 +0200

    Added SnakeCaseNamingStrategy

--- Source/Model/LoraMessage.cs
+++ Source/Model/LoraMessage.cs
@@ -1,9 +1,13 @@
 
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
 namespace RaaLabs.Edge.Connectors.Lora.Model
 {
     /// <summary>
     /// </summary>
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
     public class LoraMessage
     {
         public EndDeviceIds EndDeviceIds {get; set;}
--- Source/Model/UplinkMessage.cs
+++ Source/Model/UplinkMessage.cs
@@ -1,17 +1,19 @@
 
 
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace RaaLabs.Edge.Connectors.Lora.Model
 {
     /// <summary>
     /// </summary>
+
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
     public class UplinkMessage
     {
-
         public string FrmPayload { get; set; }
         public Dictionary<string, dynamic> DecodedPayload { get; set; }
         public string ReceivedAt { get; set; }
-
     }
 }
