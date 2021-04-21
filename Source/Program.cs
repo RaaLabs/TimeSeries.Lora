bfa2839a3d8db413b1bc33a34d05bbb6409a0ba6
jonasmwahl <jonas.wahl@me.com>
2021-04-21 13:28:59 +0200

    Changed setup in Program.cs

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,23 +1,15 @@
 ﻿// Copyright (c) RaaLabs. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Threading.Tasks;
-using System.Diagnostics.CodeAnalysis;
 using RaaLabs.Edge.Modules.EventHandling;
 using RaaLabs.Edge.Modules.EdgeHub;
 using RaaLabs.Edge.Modules.Configuration;
 
 namespace RaaLabs.Edge.Connectors.Lora
 {
-    [ExcludeFromCodeCoverage]
     static class Program
     {
-        static void Main(string[] args)
-        {
-            Run().Wait();
-        }
-
-        private static async Task Run()
+        static void Main()
         {
             var application = new ApplicationBuilder()
                 .WithModule<EventHandling>()
@@ -28,7 +20,7 @@ namespace RaaLabs.Edge.Connectors.Lora
                 .WithType<LoraParser>()
                 .Build();
 
-            await application.Run();
+            application.Run().Wait();
         }
     }
 }
